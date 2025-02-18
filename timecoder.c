19fd457a125137740d12a6845b898d4a14d4442b
Jan Claußen <jan.claussen10@web.de>
2025-02-18 16:47:47 +0100

    Refactor slope and threshold naming

--- timecoder.c
+++ timecoder.c
@@ -605,23 +605,24 @@ static void process_mk2_bitstream(struct timecoder *tc, signed int reading) {
 	    tc->secondary.lower_slope = ema(abs(secondary_reading - tc->secondary.last_lower_reading), &tc->secondary.lower_slope, 0.01);
             float current_slope = (float) (secondary_reading - tc->secondary.last_lower_reading) / INT_MAX;
             secondary->last_lower_reading = secondary_reading;
+            float last_slope = (float) secondary->lower_slope / INT_MAX;
 
 	    if (!tc->lower_bit_flipped) {
 		    if (tc->forwards) {
-                            float last_slope = (float)FORWARD_FACTOR * secondary->lower_slope / INT_MAX;
-			    if (current_slope > last_slope && tc->lower_bit == 1) {
+                            float threshold = FORWARD_FACTOR * last_slope;
+			    if (current_slope > threshold && tc->lower_bit == 1) {
 				    tc->lower_bit = 0;
 				    tc->lower_bit_flipped = true;
-			    } else if (current_slope < -last_slope && tc->lower_bit == 0) {
+			    } else if (current_slope < -threshold && tc->lower_bit == 0) {
 				    tc->lower_bit = 1;
 				    tc->lower_bit_flipped = true;
 			    }
 		    } else {
-                            float last_slope = (float)REVERSE_FACTOR * secondary->lower_slope / INT_MAX;
-			    if (current_slope > last_slope && tc->lower_bit == 0) {
+                            float threshold = REVERSE_FACTOR * last_slope;
+			    if (current_slope > threshold && tc->lower_bit == 0) {
 				    tc->lower_bit = 1;
 				    tc->lower_bit_flipped = true;
-			    } else if (current_slope < -last_slope && tc->lower_bit == 1) {
+			    } else if (current_slope < -threshold && tc->lower_bit == 1) {
 				    tc->lower_bit = 0;
 				    tc->lower_bit_flipped = true;
 			    }
@@ -637,26 +638,27 @@ static void process_mk2_bitstream(struct timecoder *tc, signed int reading) {
 	    tc->secondary.upper_slope = ema(abs(secondary_reading - tc->secondary.last_upper_reading), &tc->secondary.upper_slope, 0.01);
             float current_slope = (float) (secondary_reading - tc->secondary.last_upper_reading) / INT_MAX;
             secondary->last_upper_reading = secondary_reading;
+            float last_slope = (float) secondary->upper_slope / INT_MAX;
 
 	    /* The bits only change when an offset jump occurs. Else the previous bit is taken  */
 	    if (!tc->upper_bit_flipped) {
 		    if (tc->forwards) {
-                            float last_slope = (float)FORWARD_FACTOR * secondary->upper_slope / INT_MAX;
+                            float threshold = FORWARD_FACTOR * last_slope;
 
-			    if (current_slope > last_slope && tc->upper_bit == 0) {
+			    if (current_slope > threshold && tc->upper_bit == 0) {
 				    tc->upper_bit = 1;
 				    tc->upper_bit_flipped = true;
-			    } else if (current_slope < -last_slope && tc->upper_bit == 1) {
+			    } else if (current_slope < -threshold && tc->upper_bit == 1) {
 				    tc->upper_bit = 0;
 				    tc->upper_bit_flipped = true;
 			    }
 		    } else {
-                            float last_slope = (float)REVERSE_FACTOR * secondary->upper_slope / INT_MAX;
+                            float threshold = REVERSE_FACTOR * last_slope;
 
-			    if (current_slope > last_slope && tc->upper_bit == 1) {
+			    if (current_slope > threshold && tc->upper_bit == 1) {
 				    tc->upper_bit = 0;
 				    tc->upper_bit_flipped = true;
-			    } else if (current_slope < -last_slope && tc->upper_bit == 0) {
+			    } else if (current_slope < -threshold && tc->upper_bit == 0) {
 				    tc->upper_bit = 1;
 				    tc->upper_bit_flipped = true;
 			    }
