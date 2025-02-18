0ab1869ad1127d508af78d72a3c49d1ea16e863b
Jan Claußen <jan.claussen10@web.de>
2025-02-18 15:41:02 +0100

    Remove Gold code-related functions

--- timecoder.c
+++ timecoder.c
@@ -327,32 +327,6 @@ static inline bits_t rev2(bits_t current, struct lfsr *def)
     return ((current << one) & mask) | l;
 }
 
-// Compute Gold Code from two LFSRs
-bits_t compute_gold_code(bits_t lfsr1, bits_t lfsr2) 
-{
-    return lfsr1 ^ lfsr2;
-}
-
-// Compute Gold Code, which is the same no matter the polarity
-bits_t stable_gold_code(bits_t lfsr1, bits_t lfsr2) 
-{                                                                     
-    bits_t xor_code = lfsr1 ^ lfsr2;
-    bits_t flipped_xor_code = ~xor_code; // Inverted version
-
-    return (xor_code < flipped_xor_code) ? xor_code : flipped_xor_code;
-}
-
-// Find positions where the Gold Code is incorrect
-bits_t find_errors(bits_t gold_code_actual, bits_t gold_code_expected) {
-    return gold_code_actual ^ gold_code_expected;
-}
-
-// Correct errors using bitwise majority voting
-bits_t correct_errors(bits_t lfsr1, bits_t lfsr2, bits_t error_mask) {
-    // Correct using a simple majority vote (assumes single-bit errors)
-    return (lfsr1 & ~error_mask) | (lfsr2 & error_mask);
-}
-
 /*
  * Where necessary, build the lookup table required for this timecode
  *
